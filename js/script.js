21c11566c7d61abb8eb0a033d89547005f417caa
Lucas Fernandes Ribeiro de Oliveira <136923413+lucas-fro@users.noreply.github.com>
2024-07-15 22:17:00 -0300

    1.0.4
    melhorias, adição de icones e adição de descrições

--- js/script.js
+++ js/script.js
@@ -1,80 +1,80 @@
 const menu = {
     petisco: [
-        { nomeItem: "FRITAS", preco: 25.00 },
-        { nomeItem: "PANCETA", preco: 30.00 },
-        { nomeItem: "FRANGO FRITO + FRITAS", preco: 35.00 },
-        { nomeItem: "COXINHA DE FRANGO", preco: 35.00 },
-        { nomeItem: "BOLINHO DE QUEIJO", preco: 35.00 },
-        { nomeItem: "CHORIPAN", preco: 35.00 },
-        { nomeItem: "SANDUÍCHE DE PERNIL", preco: 35.00 },
-        { nomeItem: "HOT DOG", preco: 22.00 },
-        { nomeItem: "SANDUÍCHE DE FRANGO", preco: 35.00 },
-        { nomeItem: "NOSSO BURGUER", preco: 40.00 }
+        { nomeItem: "FRITAS", preco: 25.00, descricao: " " },
+        { nomeItem: "PANCETA", preco: 30.00, descricao: " " },
+        { nomeItem: "FRANGO FRITO + FRITAS", preco: 35.00, descricao: " " },
+        { nomeItem: "COXINHA DE FRANGO", preco: 35.00, descricao: " " },
+        { nomeItem: "BOLINHO DE QUEIJO", preco: 35.00, descricao: " " },
+        { nomeItem: "CHORIPAN", preco: 35.00, descricao: " " },
+        { nomeItem: "SANDUÍCHE DE PERNIL", preco: 35.00, descricao: " " },
+        { nomeItem: "HOT DOG", preco: 22.00, descricao: " " },
+        { nomeItem: "SANDUÍCHE DE FRANGO", preco: 35.00, descricao: " " },
+        { nomeItem: "NOSSO BURGUER", preco: 40.00, descricao: " " }
     ],
 
     drinks: [
-        { nomeItem: "JAMESON TEA & LIME", preco: 35.00 },
-        { nomeItem: "CAIPIRINHA", preco: 25.00 },
-        { nomeItem: "CAIPIROSKA NACIONAL (NUDY)", preco: 30.00 },
-        { nomeItem: "CAIPIROSKA IMPORTADA", preco: 40.00 },
-        { nomeItem: "SAKERINHA", preco: 30.00 },
-        { nomeItem: "GIN TÔNICA NACIONAL (NUDY)", preco: 30.00 },
-        { nomeItem: "GIN TÔNICA IMPORTADA", preco: 40.00 },
-        { nomeItem: "NEGRONI", preco: 35.00 },
-        { nomeItem: "APEROL SPRITZ", preco: 35.00 },
-        { nomeItem: "WHISKEY JIM BEAM DOSE", preco: 30.00 },
-        { nomeItem: "WHISKEY JACK DANIEL'S DOSE", preco: 40.00 },
-        { nomeItem: "WHISKEY HOWLER HEAD DOSE", preco: 40.00 },
-        { nomeItem: "JAGERMEISTER 40 ML", preco: 20.00 },
-        { nomeItem: "CAIPINANA FRUTAS VERMELHAS", preco: 35.00 }
+        { nomeItem: "JAMESON TEA & LIME", preco: 35.00, descricao: "(Jameson, chá gelado, suco de limão)" },
+        { nomeItem: "CAIPIRINHA", preco: 25.00, descricao: "(Cachaça, limão, açúcar)" },
+        { nomeItem: "CAIPIROSKA NACIONAL (NUDY)", preco: 30.00, descricao: "(Vodka nacional, limão, açúcar)" },
+        { nomeItem: "CAIPIROSKA IMPORTADA", preco: 40.00, descricao: "(Vodka importada, limão, açúcar)" },
+        { nomeItem: "SAKERINHA", preco: 30.00, descricao: "(Saquê, limão, açúcar)" },
+        { nomeItem: "GIN TÔNICA NACIONAL (NUDY)", preco: 30.00, descricao: "(Gin nacional, água tônica, limão)" },
+        { nomeItem: "GIN TÔNICA IMPORTADA", preco: 40.00, descricao: "(Gin importado, água tônica, limão)" },
+        { nomeItem: "NEGRONI", preco: 35.00, descricao: "(Gin, vermute rosso, Campari)" },
+        { nomeItem: "APEROL SPRITZ", preco: 35.00, descricao: "(Aperol, prosecco, água com gás)" },
+        { nomeItem: "WHISKEY JIM BEAM DOSE", preco: 30.00, descricao: "(Dose de Jim Beam)" },
+        { nomeItem: "WHISKEY JACK DANIEL'S DOSE", preco: 40.00, descricao: "(Dose de Jack Daniel's)" },
+        { nomeItem: "WHISKEY HOWLER HEAD DOSE", preco: 40.00, descricao: "(Dose de Howler Head)" },
+        { nomeItem: "JAGERMEISTER 40 ML", preco: 20.00, descricao: "(Dose de Jägermeister)" },
+        { nomeItem: "CAIPINANA FRUTAS VERMELHAS", preco: 35.00, descricao: "(Cachaça, frutas vermelhas, açúcar)" }
     ],
     
     cerveja: [
-        { nomeItem: "Chopp Brahma Claro", preco: 11.90 },
-        { nomeItem: "Chopp Colorado Indica", preco: 16.90 },
-        { nomeItem: "Chopp Stella Artois", preco: 13.90 },
-        { nomeItem: "Beck's Pilsner LN", preco: 15.90 },
-        { nomeItem: "Corona LN", preco: 18.90 },
-        { nomeItem: "Budweiser LN", preco: 14.90 },
-        { nomeItem: "Spaten LN", preco: 14.90 },
-        { nomeItem: "Stella Artois LN", preco: 14.90 },
-        { nomeItem: "Stella Artois Pure Gold", preco: 14.90 },
-        { nomeItem: "Budweiser Zero LN", preco: 14.90 }
+        { nomeItem: "Chopp Brahma Claro", preco: 11.90, descricao: " " },
+        { nomeItem: "Chopp Colorado Indica", preco: 16.90, descricao: " " },
+        { nomeItem: "Chopp Stella Artois", preco: 13.90, descricao: " " },
+        { nomeItem: "Beck's Pilsner LN", preco: 15.90, descricao: " " },
+        { nomeItem: "Corona LN", preco: 18.90, descricao: " " },
+        { nomeItem: "Budweiser LN", preco: 14.90, descricao: " " },
+        { nomeItem: "Spaten LN", preco: 14.90, descricao: " " },
+        { nomeItem: "Stella Artois LN", preco: 14.90, descricao: " " },
+        { nomeItem: "Stella Artois Pure Gold", preco: 14.90, descricao: " " },
+        { nomeItem: "Budweiser Zero LN", preco: 14.90, descricao: " " }
     ],
 
     espetinho: [
-        { nomeItem: "Carne", preco: 11.90 },
-        { nomeItem: "Coração", preco: 11.90 },
-        { nomeItem: "Frango com legumes", preco: 11.90 },
-        { nomeItem: "Kafta", preco: 11.90 },
-        { nomeItem: "Linguiça Toscana", preco: 11.90 }
+        { nomeItem: "Carne", preco: 11.90, descricao: " " },
+        { nomeItem: "Coração", preco: 11.90, descricao: " " },
+        { nomeItem: "Frango com legumes", preco: 11.90, descricao: " " },
+        { nomeItem: "Kafta", preco: 11.90, descricao: " " },
+        { nomeItem: "Linguiça Toscana", preco: 11.90, descricao: " " }
     ],
 
     SemAlcool: [
-        { nomeItem: "Água", preco: 6.90 },
-        { nomeItem: "Água com gás", preco: 6.90 },
-        { nomeItem: "H2O", preco: 11.90 },
-        { nomeItem: "Refrigerante", preco: 8.90 },
-        { nomeItem: "Suco de Abacaxi", preco: 12.90 },
-        { nomeItem: "Suco de Acerola", preco: 12.90 },
-        { nomeItem: "Suco de Acerola c/ laranja", preco: 14.90 },
-        { nomeItem: "Suco de Laranja", preco: 12.90 },
-        { nomeItem: "Suco de Limão", preco: 12.90 },
-        { nomeItem: "Limonada suíça", preco: 14.90 },
-        { nomeItem: "Suco de Manga", preco: 12.90 },
-        { nomeItem: "Suco de Maracujá", preco: 12.90 },
-        { nomeItem: "Suco de Melancia", preco: 12.90 },
-        { nomeItem: "Suco de Morango", preco: 12.90 },
-        { nomeItem: "Morango c/ leite", preco: 14.90 }
+        { nomeItem: "Água", preco: 6.90, descricao: " " },
+        { nomeItem: "Água com gás", preco: 6.90, descricao: " " },
+        { nomeItem: "H2O", preco: 11.90, descricao: " " },
+        { nomeItem: "Refrigerante", preco: 8.90, descricao: " " },
+        { nomeItem: "Suco de Abacaxi", preco: 12.90, descricao: " " },
+        { nomeItem: "Suco de Acerola", preco: 12.90, descricao: " " },
+        { nomeItem: "Suco de Acerola c/ laranja", preco: 14.90, descricao: " " },
+        { nomeItem: "Suco de Laranja", preco: 12.90, descricao: " " },
+        { nomeItem: "Suco de Limão", preco: 12.90, descricao: " " },
+        { nomeItem: "Limonada suíça", preco: 14.90, descricao: " " },
+        { nomeItem: "Suco de Manga", preco: 12.90, descricao: " " },
+        { nomeItem: "Suco de Maracujá", preco: 12.90, descricao: " " },
+        { nomeItem: "Suco de Melancia", preco: 12.90, descricao: " " },
+        { nomeItem: "Suco de Morango", preco: 12.90, descricao: " " },
+        { nomeItem: "Morango c/ leite", preco: 14.90, descricao: " " }
     ]
 };
+
 
 // Função para formatar o preço com vírgula
 function formatarPreco(preco) {
     return preco.toFixed(2).replace('.', ',');
 }
 
-// Função para exibir os itens de porção no HTML
 // Função genérica para exibir itens no HTML
 function mostrarItens(menuTipo, elementoId) {
     const container = document.getElementById(elementoId);
@@ -87,6 +87,7 @@ function mostrarItens(menuTipo, elementoId) {
             <span class="nomeItem">${item.nomeItem}</span>
             <span class="separador"></span>
             <span class="preco">R$ ${formatarPreco(item.preco)}</span>
+            <span class="descricao">${item.descricao}</span>
         `;
 
         container.appendChild(divItem);
@@ -99,4 +100,3 @@ mostrarItens('drinks', 'itens-drinks');
 mostrarItens('cerveja', 'itens-cerveja');
 mostrarItens('espetinho', 'itens-espetinho');
 mostrarItens('SemAlcool', 'itens-sem-alcool');
-
